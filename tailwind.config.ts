b8c63208a0f8003bcf22f4e3007cc87971e17f0f
= <aaronclaes22@gmail.com>
2024-06-17 18:19:51 +0200

    fix: styling issue fumadocs x tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,17 @@
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
+import { createPreset } from "fumadocs-ui/tailwind-plugin";
 
 export default {
   darkMode: "class",
-  content: ["./src/**/*.tsx"],
+  content: [
+    "./node_modules/fumadocs-ui/dist/**/*.js",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/app/**/*.{ts,tsx}",
+    "./src/content/**/*.mdx",
+    "./src/mdx-components.tsx",
+  ],
+  presets: [createPreset({ prefix: "fuma" })],
   theme: {
     extend: {
       fontFamily: {
